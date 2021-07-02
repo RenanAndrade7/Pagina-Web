38f045c92631fe1acb281879b61dce1020280e76
Renan Andrade <renan.automation@gmail.com>
2021-07-02 17:30:51 -0300

    Novas Atualizações JavaScript

---
+++ exercicios-js/fundamentos/funcao.js
@@ -0,0 +1,44 @@
+/*
+
+// Função sem retorno
+
+function imprimirSoma(a, b)
+{
+    console.log(a + b)
+}
+
+imprimirSoma(2,3)
+
+// Função com retorno
+
+function soma(a, b = 0)
+{
+    return a + b
+}
+
+console.log(soma(2,3))
+
+*/
+
+// Armazenando uma função em uma variável
+
+const imprimirSoma = function(a,b)
+{
+    console.log(a+b)
+}
+
+imprimirSoma(2,3)
+
+//Armazenando uma função arrow em uma variável
+
+const soma = (a,b) => {
+    return a + b
+}
+
+console.log(soma(2,3))
+
+// Retorno implicito
+
+const subtracao = (a,b) => a - b
+console.log(subtracao(2,3))
+
